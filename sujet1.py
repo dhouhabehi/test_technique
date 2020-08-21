35383e66b4e2e5a30467534d154d0d289fafc56b
Dhouha Behi Sefi <60827783+dhouhabehi@users.noreply.github.com>
2020-08-21 13:28:16 +0100

    Delete sujet1.py

--- sujet1.py
+++
@@ -1,6 +0,0 @@
-
-def Majuscule (chaine):
-    for i in range (0, len(chaine)):
-        if ((int(ord(chaine[i]))>=97) & (int(ord(chaine[i])<= 122))):
-            chaine[i] =  chr(ord(chaine[i]) -32)
-    return chaine
